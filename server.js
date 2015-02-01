aba3274cda2004dfc3c26ccffe585aaf272cf47d
Anastasia Zotova <azotova@gmail.com>
2015-01-31 19:53:25 -0800

    Added the command to merge pull request

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var parser = require('body-parser');
-
+var request = require('request');
 
 var port = process.env.PORT || 3000;
 
@@ -12,14 +12,20 @@ server.use(parser.json());
 
 
 server.post('/listeners', function (req, res) {
+  res.sendStatus(201);
   if (req.body.action === 'opened' || req.body.action === 'updated') {
-  	console.log("user", req.body.pull_request.user);
+  	/*console.log("user", req.body.pull_request.user);
     console.log("head", req.body.pull_request.head);
-    console.log("base", req.body.pull_request.base);
+    console.log("base", req.body.pull_request.base);*/
+    if (req.body.pull_request.head.user.login === req.body.pull_request.base.ref) {
+      var number = req.body.pull_request.number;
+      var query = 'https://api.github.com/repos/codingfitness/codingfitness/pulls/'+number+'/merge';
+      request.put(query, function (error, response, body) {
+  	    console.log("response", response.statusCode);
+      });
+    } 
   }
 });
-// server.post('/listeners', handler.post);
-
 
 console.log('Listening on port' + port);
 server.listen(port);
