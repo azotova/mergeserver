5ea71424a983aa6abca796e74869b2d61af2f3d5
Anastasia Zotova <azotova@gmail.com>
2015-01-31 18:16:33 -0800

    Changed the server

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ server.use(parser.json());
 
 
 server.post('/listeners', function (req, res) {
-  console.log("show", req);
+  if (req.body.action === 'opened' || req.body.action === 'updated') {
+  	console.log("user", req.body.pull_request.user);
+    console.log("head", req.body.pull_request.head);
+    console.log("base", req.body.pull_request.base);
+  }
 });
 // server.post('/listeners', handler.post);
 
