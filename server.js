bb69a78b8468440dd492dc19bdf1e524239c23ee
Anastasia Zotova <azotova@gmail.com>
2015-01-31 23:22:50 -0800

    Diagnostics

--- server.js
+++ server.js
@@ -41,10 +41,10 @@ var token;
 server.post('/listeners', function (req, res) {
   res.sendStatus(201);
   if (req.body.action === 'opened' || req.body.action === 'updated') {
-  	// console.log("pullrequest", req.body.pull_request);
+  	console.log("pullrequest", req.body.pull_request);
     if (req.body.pull_request.head.user.login === req.body.pull_request.base.ref) {
       var number = req.body.pull_request.number;
-      var query = 'https://api.github.com/repos/codingfitness/codingfitness/pulls/'+number+'/merge?client_id='+var1+'&client_secret='+var2;
+      var query = 'https://api.github.com/repos/codingfitness/codingfitness/pulls/'+number+'?client_id='+var1+'&client_secret='+var2;
       console.log("query", query);
       var options = {
         url: query,
@@ -52,7 +52,7 @@ server.post('/listeners', function (req, res) {
           'User-Agent': 'mergeserver'
         }
       };
-      request.put(options, function (error, response, body) {
+      request.get(options, function (error, response, body) {
       	if (error) {
       	  console.log("error", error);
       	}
