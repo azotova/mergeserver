b676dad0173c3767ec3bea6ad7f624673a722d17
Anastasia Zotova <azotova@gmail.com>
2015-01-31 20:00:10 -0800

    Added diagnostic consolelogs

--- server.js
+++ server.js
@@ -14,12 +14,11 @@ server.use(parser.json());
 server.post('/listeners', function (req, res) {
   res.sendStatus(201);
   if (req.body.action === 'opened' || req.body.action === 'updated') {
-  	/*console.log("user", req.body.pull_request.user);
-    console.log("head", req.body.pull_request.head);
-    console.log("base", req.body.pull_request.base);*/
+  	console.log("pullrequest", req.body.pull_request);
     if (req.body.pull_request.head.user.login === req.body.pull_request.base.ref) {
       var number = req.body.pull_request.number;
       var query = 'https://api.github.com/repos/codingfitness/codingfitness/pulls/'+number+'/merge';
+      console.log("query", query);
       request.put(query, function (error, response, body) {
   	    console.log("response", response.statusCode);
       });
